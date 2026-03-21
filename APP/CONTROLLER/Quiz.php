c158730475bf434e01dc339956c791eb98d70575
deepak <code.deepak9899sharma@gmail.com>
2026-03-21 20:48:00 +0530

    added Quiz API config page for students

--- APP/CONTROLLER/Quiz.php
+++ APP/CONTROLLER/Quiz.php
@@ -1,6 +1,7 @@
 <?php
 
 class Quiz extends Controller{
+    private $api = "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple";
     public function __construct() {
         $user_role = Authorization::get_user_role();
         if(!($user_role == "students")){
--- APP/VIEW/QUIZ/home.view.php
+++ APP/VIEW/QUIZ/home.view.php
@@ -1,1 +1,192 @@
-<h1>Quiz page</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students - Quiz</title>
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="<?= ROOT ?>\PUBLIC\ASSETS\imgs\favicon.png" type="image/x-icon">
+    <!-- CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.9.0/fonts/remixicon.css" rel="stylesheet" />
+    <!-- Styles -->
+    <link rel="stylesheet" href="<?= ROOT ?>\PUBLIC\ASSETS\styles\main.css">
+    <link rel="stylesheet" href="<?= ROOT ?>\PUBLIC\ASSETS\styles\style.css">
+    <link rel="stylesheet" href="<?= ROOT ?>\PUBLIC\ASSETS\styles\animation.css">
+    <link rel="stylesheet" href="<?= ROOT ?>\PUBLIC\ASSETS\styles\utility.css">
+    <link rel="stylesheet" href="<?= ROOT ?>\PUBLIC\ASSETS\styles\component.css">
+</head>
+
+<body class="relative text-[clamp(0.8rem,1.3vw,1rem)]">
+    <header class="fixed w-[100vw] z-[999]">
+        <nav class=" backdrop-filter backdrop-blur-sm  flex justify-between items-center gap-[4vw] p-2 px-[6vw]">
+            <img class="size-[70px] " src="<?= ROOT ?>\PUBLIC\ASSETS\imgs\logo.png" alt="Quiz Tracker Logo"
+                class="logo">
+            <ul class="xcenter flex justify-between items-center gap-4 p-4 hidden md:flex">
+                <li>
+                    <a href="<?= ROOT ?>/public/students/"
+                        class="flex gap-2 justify-center item-center text-black hover:text-blue-500 truncate">
+                        <i class="ri-dashboard-horizontal-fill text-[1.6em] lg:text-[1em]"></i> <span
+                            class="hidden lg:block">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= ROOT ?>/public/students/result"
+                        class="flex gap-2 justify-center item-center text-black hover:text-blue-500 truncate">
+                        <i class="ri-a-b text-[1.6em] lg:text-[1em]"></i> <span class="hidden lg:block">Result</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= ROOT ?>/public/students/leaderboard"
+                        class="flex gap-2 justify-center item-center text-black hover:text-blue-500 truncate">
+                        <i class="ri-bar-chart-fill text-[1.6em] lg:text-[1em]"></i> <span
+                            class="hidden lg:block">Leaderboard</span>
+                    </a>
+                </li>
+            </ul>
+
+            <div class="cta-buttons flex justify-between items-center gap-2 p-2 ">
+                <a href="<?= ROOT ?>\public\auth\logout"
+                    class="flex gap-2 justify-center item-center bg-white btn hover:scale-105 w-fit">
+                    <i class="ri-logout-box-line"></i> <span class="hidden lg:block">Logout</span>
+                </a>
+                <a href="<?= ROOT ?>\public\students\profile"
+                    class="flex gap-2 justify-center item-center btn btn-primary hover:scale-105 w-fit">
+                    <i class="ri-user-fill"></i> <span class="hidden lg:block">Profile</span>
+                </a>
+
+                <div class="responsive-nav-cont">
+                    <i class="ri-menu-fill text-[1.7em] cursor-pointer hover:text-orange-600 md:hidden"></i>
+                    <i class="ri-close-large-line text-[1.7em] cursor-pointer hover:text-orange-600 hidden"></i>
+                </div>
+            </div>
+        </nav>
+
+        <ul
+            class="responsive-nav-cont backdrop-blur-md border-2 border-orange-600 w-fit p-8 flex gap-4 flex-col justify-start items-start fixed right-[5vw] rounded-lg hidden ">
+            <!-- TODO : with jQuery make it toggle on menu icon click  -->
+            <li>
+                <a href="<?= ROOT ?>/public/students "
+                    class="flex gap-2 justify-center item-center text-black hover:text-blue-500 truncate">
+                    <i class="ri-dashboard-horizontal-fill text-[1.6em] lg:text-[1em] "></i> Home
+                </a>
+            </li>
+            <li>
+                <a href="<?= ROOT ?>/public/students/result"
+                    class="flex gap-2 justify-center item-center text-black hover:text-blue-500 truncate">
+                    <i class="ri-a-b text-[1.6em] lg:text-[1em] "></i> Result
+                </a>
+            </li>
+            <li>
+                <a href="<?= ROOT ?>/public/students/leaderboard"
+                    class="flex gap-2 justify-center item-center text-black hover:text-blue-500 truncate">
+                    <i class="ri-bar-chart-fill text-[1.6em] lg:text-[1em] "></i> Leaderboard
+                </a>
+            </li>
+        </ul>
+
+    </header>
+
+    <section id="hero" class="py-4 min-h-screen flex flex-col items-center justify-center">
+        <div id="quiz-config" class="flex flex-col items-center justify-start">
+            <h1 class="text-center mb-[-0.8em] text-[clamp(2em,3.4vw,2.8em)] flex justify-center items-center gap-4"> Configure Your <img class="size-[clamp(3em,5vw,7vw)] " src="<?= ROOT ?>\PUBLIC\ASSETS\imgs\logo.png" alt="Quiz Tracker Logo"></h1>
+            <p class="my-2">Choose a category, difficulty </p>
+
+            <form action="" method="post" class="border-2 border-gray shadow-lg rounded-lg bg-white flex justify-center items-center flex-col p-4">
+
+                <div class=" w-full p-4 my-2 flex justify-center items-center gap-2 flex-col md:flex-row">
+                    <label class="text-[clamp(1em,1.3vw,2.4vw)] truncate w-fit shrink-0" for="category"><i class="ri-book-shelf-fill"></i> Select Category: </label>
+                    <select name="category" class="border-2 border-orange-600 px-8 py-4 active:border-orange-600  focus:border-orange-600 py-2 px-4 rounded-lg  w-full flex justify-center items-center">
+                        <option value="any">Any Category</option>
+                        <option value="9">General Knowledge</option>
+                        <option value="10">Entertainment: Books</option>
+                        <option value="11">Entertainment: Film</option>
+                        <option value="12">Entertainment: Music</option>
+                        <option value="13">Entertainment: Musicals &amp; Theatres</option>
+                        <option value="14">Entertainment: Television</option>
+                        <option value="15">Entertainment: Video Games</option>
+                        <option value="16">Entertainment: Board Games</option>
+                        <option value="17">Science &amp; Nature</option>
+                        <option value="18">Science: Computers</option>
+                        <option value="19">Science: Mathematics</option>
+                        <option value="20">Mythology</option>
+                        <option value="21">Sports</option>
+                        <option value="22">Geography</option>
+                        <option value="23">History</option>
+                        <option value="24">Politics</option>
+                        <option value="25">Art</option>
+                        <option value="26">Celebrities</option>
+                        <option value="27">Animals</option>
+                        <option value="28">Vehicles</option>
+                        <option value="29">Entertainment: Comics</option>
+                        <option value="30">Science: Gadgets</option>
+                        <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+                        <option value="32">Entertainment: Cartoon &amp; Animations</option>
+                    </select>
+                </div>
+
+
+                <div class=" w-full p-4 my-2 flex justify-center items-center gap-2 flex-col md:flex-row">
+                    <label class="text-[clamp(1em,1.3vw,2.4vw)] truncate w-fit shrink-0"><i class="ri-signal-cellular-1-fill"></i> Select Difficulty: </label>
+                    <div class="w-full flex justify-center items-center gap-4">
+                        <input class="radio-elm" type="radio" name="difficulty" value="easy" id="easy">
+                        <label class="btn w-fit hover:bg-orange-600 hover:text-white" for="easy"><i class="ri-baseball-fill text-[1.2em]"></i> Easy</label>
+
+                        <input class="radio-elm" type="radio" name="difficulty" value="medium" id="medium">
+                        <label class="btn w-fit hover:bg-orange-600 hover:text-white" for="medium"><i class="ri-game-fill text-[1.2em]"></i> Medium</label>
+
+                        <input class="radio-elm" type="radio" name="difficulty" value="hard" id="hard">
+                        <label class="btn w-fit hover:bg-orange-600 hover:text-white" for="hard"><i class="ri-game-2-fill text-[1.2em]"></i> Hard</label>
+                    </div>
+                    
+                </div>
+
+                <button class="btn btn-primary font-bold w-fit px-8" type="submit"><i class="ri-play-large-fill"></i> Start Quiz</button>
+            </form>
+        </div>
+    </section>
+
+    <!-- FOOTER -->
+    <footer class="bg-black text-white mt-10">
+        <div class="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-3 gap-8">
+
+            <!-- About -->
+            <div>
+                <img class="size-[100px] " src="<?= ROOT ?>\PUBLIC\ASSETS\imgs\dark-logo.png" alt="Quiz Tracker Logo">
+                <p class="text-sm leading-relaxed">
+                    Test your knowledge with interactive quizzes.
+                    Improve skills, track progress, and challenge yourself daily.
+                </p>
+            </div>
+
+            <!-- Quick Links -->
+            <div>
+                <h2 class="text-white text-lg font-semibold mb-4">Quick Links</h2>
+                <ul class="space-y-2 text-sm">
+                    <li><a href="#" ">Home</a></li>
+                    <li><a href=" #" ">Quizzes</a></li>
+                    <li><a href=" #" ">Results</a></li>
+                    <li><a href=" #" ">Contact</a></li>
+                </ul>
+            </div>
+
+            <!-- Contact -->
+            <div>
+                <h2 class=" text-white text-lg font-semibold mb-4">Contact</h2>
+                            <p class="text-sm">Email: code.deepak9899sharma@gmail.com</p>
+                            <p class="text-sm mt-2">Phone: +91 92059 75484</p>
+            </div>
+
+        </div>
+
+        <!-- Bottom Bar -->
+        <div class="border-t border-gray-700">
+            <div class="max-w-7xl mx-auto px-6 py-4 text-center text-sm">
+                © 2026 Quiz. All Rights Reserved.
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
